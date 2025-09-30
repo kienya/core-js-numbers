5dbad2be3bf0ecda18d04381ae5f5e9f889d802a
Konstantin Kienya <kienyamail@gmail.com>
2025-09-30 10:34:41 +0300

    feat: add task solution getSumOfDigits

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -280,8 +280,11 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  return num
+    .toString()
+    .split('')
+    .reduce((acc, item) => acc + Number(item), 0);
 }
 
 /**
